b1459da004e6bc6184947580644e14edae9f28cb
Kelvin Jardin <kelvin.jardin@dataplan.co.uk>
2024-09-07 15:40:31 +0100

    main - wip

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME="registry.byjardin.co.uk/kelv/freestontots/app:latest"
+IMAGE_NAME="registry.byjardin.co.uk/kelv/freestontots:latest"
 
 # Build the Docker image
 docker build -t $IMAGE_NAME .
