1d545514a174d543ddd0c3b6619083f20a7d3292
Kelvin Jardin <kelvin.jardin@dataplan.co.uk>
2024-09-08 21:05:07 +0100

    main - remove unused middleware

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,3 @@
-import { handlers } from "@/app/auth"; // Referring to the auth.ts we just created
+import { handlers } from "@/app/auth";
 
-export const { GET, POST } = handlers;
-export { auth as middleware } from "@/app/auth";
+export const { GET, POST } = handlers
