fc32a8294ab4643a5884749561d0c60371d5a956
Kelvin Jardin <kelvin.jardin@dataplan.co.uk>
2024-09-15 17:17:04 +0100

    main - favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,22 @@ import "@/styles/tailwind.css";
 import "../styles/index.css";
 import "../styles/font.css";
 
-function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="theme-color" content="#000000" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <body>{children}</body>
-    </html>
-  );
+function RootLayout({children}: { children: ReactNode }) {
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <meta name="theme-color" content="#000000"/>
+                <link rel="manifest" href="/manifest.json"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+            </head>
+            <body>{children}</body>
+        </html>
+    );
 }
+
 export default RootLayout;
