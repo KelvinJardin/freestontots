24a5ba53a9fab18b7cf9ab6814cfed4a57f412b5
Kelvin Jardin <kelvin.jardin@dataplan.co.uk>
2024-09-15 17:44:03 +0100

    main - better sizing on gallery

--- src/app/Gallery.tsx
+++ src/app/Gallery.tsx
@@ -5,29 +5,31 @@ import Carousel from "react-material-ui-carousel";
 import Image from "next/image";
 
 const images = [
-  "img_inside1.jpg",
-  "img_inside2.jpg",
-  "img_inside3.jpg",
-  "img_outside1.jpg",
-  "img_outside2.jpg",
-  "img_outside3.jpg",
-  "img_outside4.jpg",
+    "img_inside1.jpg",
+    "img_inside2.jpg",
+    "img_inside3.jpg",
+    "img_outside1.jpg",
+    "img_outside2.jpg",
+    "img_outside3.jpg",
+    "img_outside4.jpg",
 ]
 
 export default function Gallery() {
-  return (
-    <Carousel sx={{width: '650px', height: '550px'}}>
-      {
-        images.map( (item, i) =>
-          <Image
-            key={i}
-            src={`/images/${item}`}
-            alt={`image ${i}`}
-            width={650}
-            height={550}
-          />
-        )
-      }
-    </Carousel>
-  );
+    return (
+        <div className="w-5/6">
+            <Carousel>
+                {
+                    images.map((item, i) =>
+                        <Image
+                            key={i}
+                            src={`/images/${item}`}
+                            alt={`image ${i}`}
+                            width={650}
+                            height={550}
+                        />
+                    )
+                }
+            </Carousel>
+        </div>
+    );
 }
